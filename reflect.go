47b710f4b622a65dd5e0a3d1bfdb40dab0c8db89
Jon Calhoun <joncalhoun@gmail.com>
2018-07-02 12:07:16 -0400

    Code cleanup in reflect.go
    reflect.go's field function was pretty large so I tried to break it
    into a few more logical smaller functions to reduce complexity. I'm
    still only testing the field function and relying on it calling
    other functions to test them, as the only thing I really care about
    is that the fields function works as expected however that gets
    done.

--- reflect.go
+++ reflect.go
@@ -1,13 +1,18 @@
 package form
 
 import (
-	"fmt"
 	"html/template"
 	"reflect"
 	"strings"
 )
 
-func fields(v interface{}, name ...string) []field {
+// valueOf is basically just reflect.ValueOf, but if the Kind() of the
+// value is a pointer or interface it will try to get the reflect.Value
+// of the underlying element, and if the pointer is nil it will
+// create a new instance of the type and return the reflect.Value of it.
+//
+// This is used to make the rest of the fields function simpler.
+func valueOf(v interface{}) reflect.Value {
 	rv := reflect.ValueOf(v)
 	// If a nil pointer is passed in but has a type we can recover, but I
 	// really should just panic and tell people to fix their shitty code.
@@ -19,8 +24,14 @@ func fields(v interface{}, name ...string) []field {
 	for rv.Kind() == reflect.Ptr || rv.Kind() == reflect.Interface {
 		rv = rv.Elem()
 	}
+	return rv
+}
+
+func fields(v interface{}, names ...string) []field {
+	rv := valueOf(v)
 	if rv.Kind() != reflect.Struct {
-		fmt.Println(rv.Kind())
+		// We can't really do much with a non-struct type. I suppose this
+		// could eventually support maps as well, but for now it does not.
 		panic("invalid value; only structs are supported")
 	}
 
@@ -28,8 +39,10 @@ func fields(v interface{}, name ...string) []field {
 	ret := make([]field, 0, t.NumField())
 	for i := 0; i < t.NumField(); i++ {
 		rf := rv.Field(i)
-		// If this is a nil pointer, create a new instance of the element
-		// type that it points to so we can recur easier.
+		// If this is a nil pointer, create a new instance of the element.
+		// This could probably be done in a simpler way given that we
+		// typically recur with this value, but this works so I'm letting it
+		// be.
 		if t.Field(i).Type.Kind() == reflect.Ptr && rf.IsNil() {
 			rf = reflect.New(t.Field(i).Type.Elem()).Elem()
 		}
@@ -39,7 +52,7 @@ func fields(v interface{}, name ...string) []field {
 		// to provide the name of this struct field to be added as a prefix
 		// to the fields.
 		if rf.Kind() == reflect.Struct {
-			ret = append(ret, fields(rf.Interface(), append(name, t.Field(i).Name)...)...)
+			ret = append(ret, fields(rf.Interface(), append(names, t.Field(i).Name)...)...)
 			continue
 		}
 
@@ -51,46 +64,43 @@ func fields(v interface{}, name ...string) []field {
 		if _, ok := tags["-"]; ok {
 			continue
 		}
-
-		fieldName := append(name, t.Field(i).Name)
+		name := append(names, t.Field(i).Name)
 		f := field{
-			Name:        strings.Join(fieldName, "."),
+			Name:        strings.Join(name, "."),
 			Label:       t.Field(i).Name,
 			Placeholder: t.Field(i).Name,
 			Type:        "text",
 			Value:       rv.Field(i).Interface(),
 		}
-
-		if v, ok := tags["name"]; ok {
-			f.Name = v
-		}
-		if v, ok := tags["label"]; ok {
-			f.Label = v
-			// DO NOT move this label check after the placeholder check or
-			// this will cause issues.
-			f.Placeholder = v
-		}
-		if v, ok := tags["placeholder"]; ok {
-			f.Placeholder = v
-		}
-		if v, ok := tags["type"]; ok {
-			f.Type = v
-		}
-		if v, ok := tags["id"]; ok {
-			f.ID = v
-		}
-		if v, ok := tags["footer"]; ok {
-			// Probably shouldn't be HTML but whatever.
-			f.Footer = template.HTML(v)
-		}
-
-		// The "-" tag is special and signified a field to ignore
-		// and not add to your list of fields.
-		if _, ok := tags["-"]; !ok {
-			ret = append(ret, f)
-		}
+		applyTags(&f, tags)
+		ret = append(ret, f)
 	}
 	return ret
+}
+
+func applyTags(f *field, tags map[string]string) {
+	if v, ok := tags["name"]; ok {
+		f.Name = v
+	}
+	if v, ok := tags["label"]; ok {
+		f.Label = v
+		// DO NOT move this label check after the placeholder check or
+		// this will cause issues.
+		f.Placeholder = v
+	}
+	if v, ok := tags["placeholder"]; ok {
+		f.Placeholder = v
+	}
+	if v, ok := tags["type"]; ok {
+		f.Type = v
+	}
+	if v, ok := tags["id"]; ok {
+		f.ID = v
+	}
+	if v, ok := tags["footer"]; ok {
+		// Probably shouldn't be HTML but whatever.
+		f.Footer = template.HTML(v)
+	}
 }
 
 func parseTags(tags string) map[string]string {
